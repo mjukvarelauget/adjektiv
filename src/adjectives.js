a11656a247767cbefbbf609c28d5ee2fe865c5e0
haved <krogstie.havard@gmail.com>
2022-01-23 15:10:08 +0100

    add adjective replacement code

--- src/adjectives.js
+++ src/adjectives.js
@@ -1,36 +1,36 @@
 
 REPLACEMENTS = {
-    "%a": ["grønn"],
-    "%at": ["grønt"],
-    "%an": ["grønne"],
-    "%ar": ["grønne"],
-    "%akomp": ["grønnere"],
-    "%asup": ["grønnest"],
-    "%ansup": ["grønneste"],
+    "a": ["grønn"],
+    "at": ["grønt"],
+    "an": ["grønne"],
+    "ar": ["grønne"],
+    "akomp": ["grønnere"],
+    "asup": ["grønnest"],
+    "ansup": ["grønneste"],
 };
 // komparativ er det samme enten det er bestemt eller ikke!
-REPLACEMENT["%ankomp"] = REPLACEMENT["%akomp"];
+REPLACEMENT["ankomp"] = REPLACEMENT["akomp"];
 
 export function addSimpleAdjective(base) {
     const neutrum = base.endsWith("t") ? base : base+"t";
-    REPLACEMENTS["%a"].append(base);
-    REPLACEMENTS["%at"].append(neutrum);
-    REPLACEMENTS["%an"].append(base+"e");
-    REPLACEMENTS["%ar"].append(base+"e");
-    REPLACEMENTS["%akomp"].append(base+"ere");
-    REPLACEMENTS["%asup"].append(base+"est");
-    REPLACEMENTS["%ansup"].append(base+"este");
+    REPLACEMENTS["a"].append(base);
+    REPLACEMENTS["at"].append(neutrum);
+    REPLACEMENTS["an"].append(base+"e");
+    REPLACEMENTS["ar"].append(base+"e");
+    REPLACEMENTS["akomp"].append(base+"ere");
+    REPLACEMENTS["asup"].append(base+"est");
+    REPLACEMENTS["ansup"].append(base+"este");
 }
 
 export function addLongAdjective(base) {
     const neutrum = base.endsWith("t") ? base : base+"t";
-    REPLACEMENTS["%a"].append(base);
-    REPLACEMENTS["%at"].append(neutrum);
-    REPLACEMENTS["%an"].append(base+"e");
-    REPLACEMENTS["%ar"].append(base+"e");
-    REPLACEMENTS["%akomp"].append("mer " + base);
-    REPLACEMENTS["%asup"].append("mest " + base);
-    REPLACEMENTS["%ansup"].append("mest " + base+"e");
+    REPLACEMENTS["a"].append(base);
+    REPLACEMENTS["at"].append(neutrum);
+    REPLACEMENTS["an"].append(base+"e");
+    REPLACEMENTS["ar"].append(base+"e");
+    REPLACEMENTS["akomp"].append("mer " + base);
+    REPLACEMENTS["asup"].append("mest " + base);
+    REPLACEMENTS["ansup"].append("mest " + base+"e");
 }
 
 // TODO call these from a list
@@ -38,21 +38,4 @@ addSimpleAdjective("smart");
 addSimpleAdjective("lur");
 addLongAdjective("intelligent");
 
-/**
- * Takes in a string with adjective codes, and replaces them with correct adjectives
- */
-export function replaceAdjectives(input, seed) {
-    const random = mulberry32(+seed);
-
-
-}
-
-// Our random generator
-function mulberry32(a) {
-    return function() {
-      var t = a += 0x6D2B79F5;
-      t = Math.imul(t ^ t >>> 15, t | 1);
-      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-      return ((t ^ t >>> 14) >>> 0) / 4294967296;
-    }
-}
+export default REPLACEMENTS;
---
+++ src/replacer.js
@@ -0,0 +1,62 @@
+import REPLACEMENTS from "./adjectives.js"
+
+/**
+ * Takes in a string with adjective codes, and replaces them with correct adjectives
+ */
+export function replaceAdjectives(input, seed) {
+    const random = mulberry32(+seed);
+
+    // Build a happy little trie
+    const trie = {};
+    for(const code in REPLACEMENTS) {
+        let head = trie;
+        for(const char of code)
+            trie[char] = (head = (trie[char] ?? {}));
+        head.replacement = REPLACEMENTS[code];
+    }
+
+    let output = "";
+    let seen = 0; // We are done with all input chars before seen
+    while(true) {
+        // Find the next %
+        const found = input.indexOf("%", seen);
+        // If there is none, we are done
+        if (found == -1) {
+            output += input.slice(seen, input.length())
+            return output;
+        }
+
+        // Add all input up to the % to the output
+        output += input.slice(seen, found);
+        let headChar = found+1;
+        let head = trie;
+        seen = headChar;
+        // Go through the trie as long as it still accepts
+        while (head.hasOwnProperty(input.charAt(headChar))) {
+            head = head[input.charAt(headChar++)];
+
+            // If the word [found+1, headChar-1] is a valid replacement,
+            // it will be our replacement, unless we find an even longer one, so update seen!
+            if (head.hasOwnProperty("replacement"))
+                seen = headChar;
+        }
+
+        if (seen == headChar) //It was not a valid replacement, put the % back in the output
+            output += "%";
+        else {
+            const code = input.slice(found+1, seen);
+            const replacements = REPLACEMENTS[code];
+            output += replacements[random() % replacements.length];
+        }
+    }
+}
+
+// Our random generator
+function mulberry32(a) {
+    return function() {
+      var t = a += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+}
