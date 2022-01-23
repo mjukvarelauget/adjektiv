9c13e7551ce24ef831542d502f2c6d63601fcf9f
Grimmbol <krogstie.oystein@gmail.com>
2022-01-23 16:20:14 +0100

    more and improved word gen

--- src/adjectives.js
+++ src/adjectives.js
@@ -8,29 +8,34 @@ let REPLACEMENTS = {
     "ansup": [],
 };
 
-let long_conj_end = ["mer", "mest", "mest -e"];
+
+// Now we declare all the words and conjugations
+let long_conj_end = ["mer-", "mest-", "mest-e"];
 
 // t, e, e, ere, est, este
 let a1_nom_end = ["t", "e", "e"];
 let a1_conj_end = ["ere", "est", "este"];
 let a1_list = ['rød', 'grønn', 'gul'];
 
-// t, e, e, mer, mest, mest -e
-let a1_long = ['abnorm'];
+// t, e, e, mer-, mest-, mest-e
+let a1_long_list = ['abnorm'];
 
-let a2 = [];
+let a2_list = [];
 
 // -, e, e, mer, mest, mest -e
-let a2_long = ['abrupt', 'absolutt', 'absolutistisk', 'adelig'];
+let a2_long_list = ['abrupt', 'absolutt', 'absolutistisk', 'adelig'];
 
-let a3 = [];
+let a3_list = [];
 
 // -, -, -, mer, mest, mest -e
-let a3_long = ['advarende'];
+let a3_long_list = ['advarende'];
 
 let categories = {
   a1: {
     list: a1_list, nom_end: a1_nom_end, conj_end: a1_conj_end
+  },
+  a1_long: {
+    list: a1_long_list, nom_end: a1_nom_end, conj_end: long_conj_end
   }
 }
 
@@ -47,12 +52,24 @@ function generateReplacements(categories) {
       REPLACEMENTS["an"].push(word + nom_end[1]);
       REPLACEMENTS["ar"].push(word + nom_end[2]);
 
-      // Conjugations
-      let prefix, postfix;
-      [prefix, postfix] = conj_end[2].split('-');
-      REPLACEMENTS["akomp"].push(word + conj_end[0]);
-      REPLACEMENTS["asup"].push(word + conj_end[1]);
-      REPLACEMENTS["ansup"].push(prefix + word + postfix);
+      // Conjugates
+      // For long adjectives, the conjugation is a prefix and possibly a postfix
+      // For short adjective, the conjugation is allways postfix
+
+      // A dash indicates a long adjective
+      let isLong = conj_end[0].indexOf('-') !== -1;
+
+      // Hard code for now
+      if(isLong) {
+	REPLACEMENTS["akomp"].push("mer " + word);
+	REPLACEMENTS["asup"].push("mest " + word);
+	REPLACEMENTS["ansup"].push("mest " + word + "e");
+      }
+      else {
+	REPLACEMENTS["akomp"].push(word + conj_end[0]);
+	REPLACEMENTS["asup"].push(word + conj_end[1]);
+	REPLACEMENTS["ansup"].push(word);
+      }
     }
   }
 }
